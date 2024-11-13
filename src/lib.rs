58bc204793591b8bff7cd54d52655f698fa7dbf2
Greedy Dev <{ID}+{username}@users.noreply.github.com>
2024-11-13 19:27:10 +0200

    Moved logic to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,96 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use thiserror::Error;
+use pest_derive::Parser;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct URIParser;
+
+pub struct ParsedURI {
+    /// URI scheme
+    pub scheme: String,
+    /// Authority -- username
+    pub username: Option<String>,
+    /// Authority -- password
+    pub password: Option<String>,
+    /// Domain component
+    pub domain: Option<String>,
+    /// IP component
+    pub ip: Option<String>,
+    pub port: Option<String>,
+    pub path: Option<String>,
+    pub query_params: Vec<(String, String)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("Can't parse URI")]
+    ParsingFailed,
+    
+    #[error("Unexpected parsing rule encountered: {0}")]
+    UnexpectedParsingRule(String),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ParsedURI {
+    pub fn parse(uri: &str) -> Result<Self, ParseError> {
+        let pairs = URIParser::parse(Rule::uri, uri)
+            .map_err(|_| ParseError::ParsingFailed)?;
+        
+        let mut scheme = String::new();
+        let mut username = None;
+        let mut password = None;
+        let mut domain = None;
+        let mut ip = None;
+        let mut port = None;
+        let mut path = None;
+        let mut query_params: Vec<(String, String)> = Vec::new();
+
+        for pair in pairs {
+            match pair.as_rule() {
+                Rule::scheme => scheme = pair.as_str().to_string(),
+                Rule::authority => {
+                    let mut authority_inner = pair.into_inner();
+                    if let Some(username_pair) = authority_inner.next() {
+                        username = Some(username_pair.as_str().to_string())
+                    }
+                    if let Some(password_pair) = authority_inner.next() {
+                        password = Some(password_pair.as_str().to_string());
+                    }
+                }
+                Rule::host => {
+                    let host_inner = pair.into_inner().next().unwrap();
+                    match host_inner.as_rule() {
+                        Rule::ipv4 => ip = Some(host_inner.as_str().to_string()),
+                        Rule::domain => domain = Some(host_inner.as_str().to_string()),
+                        _ => {}
+                    }
+                }
+                Rule::port => {
+                    let port_str = pair.as_str().trim_start_matches(':');
+                    port = Some(port_str.to_string())
+                },
+                Rule::path => path = Some(pair.as_str().to_string()),
+                Rule::query => {
+                    for key_value_pair in pair.into_inner() {
+                        let mut inner_pairs = key_value_pair.into_inner();
+                        let key = inner_pairs.next().unwrap().as_str();
+                        let value = inner_pairs.next().unwrap().as_str();
+                        query_params.push((key.to_string(), value.to_string()));
+                    }
+                }
+                rule => return Err(ParseError::UnexpectedParsingRule(format!("{:?}", rule))),
+            }
+        }
+
+        Ok(Self {
+            scheme,
+            username,
+            password,
+            domain,
+            ip,
+            port,
+            path,
+            query_params
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,48 @@
-use clap::Parser as ClapParser;
-use pest_derive::Parser as PestParser;
-use pest::Parser;
-use pest::iterators::Pairs;
+use clap::Parser;
+use uri_parser::{ParseError, ParsedURI};
 
-#[derive(ClapParser, Debug)]
+#[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// URI to parse
     #[arg()]
     uri: String,
-    
+
     /// Verbose error display
     #[arg(short, long)]
     verbose: bool,
 }
 
-#[derive(PestParser)]
-#[grammar = "grammar.pest"]
-struct URIParser;
-
-fn main() {
+fn main() -> Result<(), ParseError>{
     let args = Args::parse();
     
-    let parsed_uri_result = URIParser::parse(Rule::uri, &args.uri);
-    match parsed_uri_result {
-        Ok(parsed_uri) => {
-            process_uri(parsed_uri);
-        }
-        Err(e) => {
-            println!("Invalid URI found");
-            if args.verbose {
-                println!("{:?}", e);
-            }
-        }
-    }
-}
+    let parsed_uri = ParsedURI::parse(&args.uri)?;
 
-fn process_uri(parsed_uri: Pairs<Rule>) {
-    for pair in parsed_uri {
-        match pair.as_rule() {
-            Rule::scheme => println!("scheme: {}", pair.as_str()),
-            Rule::authority => {
-                println!("authority:");
-                let mut authority_inner = pair.into_inner();
-                if let Some(username_pair) = authority_inner.next() {
-                    println!("  - username: {}", username_pair.as_str());
-                }
-                if let Some(password_pair) = authority_inner.next() {
-                    println!("  - password: {}", password_pair.as_str());
-                }
-            }
-            Rule::host => {
-                let host_inner = pair.into_inner().next().unwrap();
-                match host_inner.as_rule() {
-                    Rule::ipv4 => println!("ip: {}", host_inner.as_str()),
-                    Rule::domain => println!("domain: {}", host_inner.as_str()),
-                    _ => {}
-                }
-            }
-            Rule::port => {
-                let port_str = pair.as_str().trim_start_matches(':');
-                println!("port: {}", port_str);
-            }
-            Rule::path => println!("path: {}", pair.as_str()),
-            Rule::query => {
-                println!("query:");
-                for key_value_pair in pair.into_inner() {
-                    let mut inner_pairs = key_value_pair.into_inner();
-                    let key = inner_pairs.next().unwrap().as_str();
-                    let value = inner_pairs.next().unwrap().as_str();
-                    println!("  - {}: {}", key, value);
-                }
-            }
-            _ => {}
-        }
+    println!("Scheme: {}", parsed_uri.scheme);
+    if let Some(username) = parsed_uri.username {
+        println!("Username: {}", username);
     }
+    if let Some(password) = parsed_uri.password {
+        println!("Password: {}", password);
+    }
+    if let Some(domain) = parsed_uri.domain {
+        println!("Domain: {}", domain);
+    }
+    if let Some(ip) = parsed_uri.ip {
+        println!("IP: {}", ip);
+    }
+    if let Some(port) = parsed_uri.port {
+        println!("Port: {}", port);
+    }
+    if let Some(path) = parsed_uri.path {
+        println!("Path: {}", path);
+    }
+    if !parsed_uri.query_params.is_empty() {
+        println!("Query params:");
+        parsed_uri.query_params.iter().for_each(|param| {
+            println!("  - {}: {}", param.0, param.1);
+        })
+    }
+    
+    Ok(())
 }
