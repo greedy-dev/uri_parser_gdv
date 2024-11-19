47b54df2777eddc6da049986ee8ebe1ddf7dbabe
Greedy Dev <{ID}+{username}@users.noreply.github.com>
2024-11-20 01:02:22 +0200

    Updated tests, made some changes to the grammar

--- tests/grammar_tests.rs
+++ tests/grammar_tests.rs
@@ -5,57 +5,145 @@ mod tests {
     use super::*;
     
     #[test]
-    fn valid_scheme() {
-        let res = URIParser::parse(Rule::scheme, "https");
-        assert!(res.is_ok());
-    }
-    
-    #[test]
-    fn invalid_scheme() {
-        let res = URIParser::parse(Rule::scheme, "1:http");
+    fn test_scheme() -> Result<(), Box<dyn std::error::Error>> {
+        let scheme = "pest-rs.scheme2+";
+        let parsed_scheme = URIParser::parse(Rule::scheme, scheme)?;
         
-        assert!(res.is_err());
+        assert_eq!(parsed_scheme.as_str(), scheme);
+        
+        Ok(())
     }
     
     #[test]
-    fn valid_host() {
-        let res = URIParser::parse(Rule::host, "greedydev.io");
-        assert!(res.is_ok());
-    }
-    
-    #[test]
-    fn invalid_host() {
-        let res = URIParser::parse(Rule::host, "123");
-        assert!(res.is_err());
-    }
-    
-    #[test]
-    fn valid_query() {
-        let res = URIParser::parse(Rule::query, "?one=1&two=2");
-        assert!(res.is_ok());
-    }
-    
-    #[test]
-    fn invalid_query() {
-        let res = URIParser::parse(Rule::query, "!one=1&two");
-        assert!(res.is_err());
-    }
-    
-    #[test]
-    fn valid_port() {
-        let res = URIParser::parse(Rule::port, ":8080");
-        assert!(res.is_ok());
+    fn test_authority_full() -> Result<(), Box<dyn std::error::Error>> {
+        let username = "testuser";
+        let password = "testpass";
+        let authority = format!("{}:{}", username, password);
+        let mut parsed_authority = URIParser::parse(Rule::authority, authority.as_str())?
+            .next()
+            .unwrap()
+            .into_inner();
+        
+        // Username and password
+        assert_eq!(parsed_authority.len(), 2);
+        
+        assert_eq!(parsed_authority.next().unwrap().as_str(), username);
+        assert_eq!(parsed_authority.next().unwrap().as_str(), password);
+        
+        Ok(())
     }
 
     #[test]
-    fn valid_uri() {
-        let res = URIParser::parse(Rule::uri, "wss://hello:world@greedydev.io:8080/hello/world?msg=one&lvl=2");
-        assert!(res.is_ok());
+    fn test_authority_only_username() -> Result<(), Box<dyn std::error::Error>> {
+        let username = "testuser";
+        let mut parsed_authority = URIParser::parse(Rule::authority, username)?
+            .next()
+            .unwrap()
+            .into_inner();
+        
+        // Only username
+        assert_eq!(parsed_authority.len(), 1);
+        assert_eq!(parsed_authority.next().unwrap().as_str(), username);
+
+        Ok(())
+    }
+    
+    #[test]
+    fn test_host() -> Result<(), Box<dyn std::error::Error>> {
+        let host = "test.greedydev.io";
+        let parsed_host = URIParser::parse(Rule::host, host)?;
+        
+        assert_eq!(parsed_host.as_str(), host);
+        
+        Ok(())
+    }
+    
+    #[test]
+    fn test_query() -> Result<(), Box<dyn std::error::Error>> {
+        let query = "?one=1&two";
+        let mut parsed_query = URIParser::parse(Rule::query, query)?
+            .next()
+            .unwrap()
+            .into_inner();
+
+        let mut first_component = parsed_query
+            .next()
+            .unwrap()
+            .into_inner();
+
+        assert_eq!(first_component.next().unwrap().as_str(), "one");
+        assert_eq!(first_component.next().unwrap().as_str(), "1");
+
+        let mut second_component = parsed_query
+            .next()
+            .unwrap()
+            .into_inner();
+
+        assert_eq!(second_component.next().unwrap().as_str(), "two");
+        
+        Ok(())
+    }
+    
+    #[test]
+    fn test_port() -> Result<(), Box<dyn std::error::Error>> {
+        let port = "8080";
+        let parsed_port = URIParser::parse(Rule::port, port)?;
+        
+        assert_eq!(parsed_port.as_str(), port);
+        
+        Ok(())
     }
 
     #[test]
-    fn invalid_uri() {
-        let res = URIParser::parse(Rule::uri, "/greedydev.io?msg=one&lvl=2");
-        assert!(res.is_err());
+    fn test_uri() -> Result<(), Box<dyn std::error::Error>> {
+        let scheme = "wss";
+        let username = "user";
+        let password = "pass";
+        let domain = "greedydev.io";
+        let port = "8080";
+        let path = "/hello/worlds";
+        let query = "?msg=one&lvl=2";
+        let uri = format!("{scheme}://{username}:{password}@{domain}:{port}{path}{query}");
+        
+        let mut parsed_uri = URIParser::parse(Rule::uri, uri.as_str())?;
+        
+        assert_eq!(parsed_uri.next().unwrap().as_str(), scheme);
+        
+        let mut authority = parsed_uri
+            .next()
+            .unwrap()
+            .into_inner();
+        
+        assert_eq!(authority.next().unwrap().as_str(), username);
+        assert_eq!(authority.next().unwrap().as_str(), password);
+        
+        assert_eq!(parsed_uri.next().unwrap().as_str(), domain);
+        assert_eq!(parsed_uri.next().unwrap().as_str(), port);
+        assert_eq!(parsed_uri.next().unwrap().as_str(), path);
+        
+        let mut parsed_query = parsed_uri
+            .next()
+            .unwrap()
+            .into_inner();
+
+        assert_eq!(parsed_query.len(), 2);
+        
+        let mut first_component = parsed_query
+            .next()
+            .unwrap()
+            .into_inner();
+
+        assert_eq!(first_component.next().unwrap().as_str(), "msg");
+        assert_eq!(first_component.next().unwrap().as_str(), "one");
+
+        let mut second_component = parsed_query
+            .next()
+            .unwrap()
+            .into_inner();
+
+        assert_eq!(second_component.next().unwrap().as_str(), "lvl");
+        assert_eq!(second_component.next().unwrap().as_str(), "2");
+
+        Ok(())
     }
 }
